ad8c2a4874599e3ba3da88c528eb716b616c914b
EmmA <emma.arlanda@viacesi.fr>
2022-04-27 14:02:24 +0200

    Link to the data project

---
+++ TRMDataManager.Library/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDataManager.Library.Models
+{
+    public class UserModel
+    {
+        public string Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string EmailAddress { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+    }
+}
