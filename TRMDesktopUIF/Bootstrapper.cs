67aabc56c95d62bb76e2548c6c860fb4f343c064
EmmA <emma.arlanda@viacesi.fr>
2022-04-28 13:40:43 +0200

    Add UI to sln
    UI woth .NetFramework

---
+++ TRMDesktopUIF/Bootstrapper.cs
@@ -0,0 +1,60 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUIF.ViewModels;
+
+namespace TRMDesktopUIF
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        //to manage instanciation of every instance that gonna be asked for
+        private SimpleContainer _container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+            //    .Singleton<IAPIHelper, APIHelper>();
+
+            //connecting viewModels to views
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType)); //le RegisterPerRequest
+        }
+
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+
+    }
+}
---
+++ TRMDesktopUIF/ViewModels/ShellViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace TRMDesktopUIF.ViewModels
+{
+    class ShellViewModel
+    {
+    }
+}
